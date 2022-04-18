bc931cec0cebbb11c04474551faddcf29ca5eb5f
Manohy Saotra RAJAONAH <86122918+manohySr@users.noreply.github.com>
2022-04-18 22:10:03 +0100

    Add files via upload

---
+++ FRONTEND/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import List from './List';
+import { Container } from '@material-ui/core';
+
+
+const App = () => {
+    return (
+        <>
+        <Container style={{ padding : '10% 0' }}>
+            <List />
+        </Container>
+        </>
+    );
+}
+
+export default App;
+// hello just testing github desktop
---
+++ FRONTEND/src/api/dist/api.dev.js
@@ -0,0 +1,16 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+var _axios = _interopRequireDefault(require("axios"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+var _default = _axios["default"].create({
+  baseURL: "http://127.0.0.1:8000/api/"
+});
+
+exports["default"] = _default;
